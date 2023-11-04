ee2441f7bb8995c981d6d33315464860e301182f
moldovexc <alexanderparpulansky@gmail.com>
2023-11-04 20:29:30 +0200

    November LOOK

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Background.css';
-import background from '../../assets/bg.mp4'
+import background from '../../assets/mario2.mp4'
 
 const Background = () => {
     return (
